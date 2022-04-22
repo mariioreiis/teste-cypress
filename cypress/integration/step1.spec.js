b3cefc3b91837f1e4de7a0133b064aa4f091af95
mariioreiis <marioreis1236@gmail.com>
2022-04-22 09:14:55 -0300

    meu primeiro commit

---
+++ cypress/integration/step1.spec.js
@@ -0,0 +1,44 @@
+describe('Step', () => {
+    it.skip('Acessar o site e logar na conta', () => {
+        cy.visit('https://coalizao.hml.fabricadigital.com.br');
+        cy.get('.cc-btn').click();
+        cy.get('.btn-account').click();
+        cy.get('#id_username').type('mario');
+        cy.get('#id_password').type('Mario@12');
+        cy.get('.btn-primary').click();
+    });
+    
+        it.skip('Acessar o site e criar uma nova conta', () => {
+            cy.visit('https://coalizao.hml.fabricadigital.com.br');
+            cy.get('.cc-btn').click();
+            cy.get('.btn-account').click();
+            cy.get(':nth-child(6) > .btn-reset').click();
+            cy.get('#id_username').type('marioreis');
+            cy.get('#id_first_name').type('Mario');
+            cy.get('#id_last_name').type('marioreis');
+            cy.get('#id_email').type('marioreis@123456.com');
+            cy.get('#id_password1').type('Lalalala12');
+            cy.get('#id_password2').type('Lalalala12');
+            //cy.get('.disclaimer-modal-link').click();
+            //cy.get('#id_disclaimer').click();
+           // cy.get('#id_recaptcha').check();
+            });
+
+            it('Exportar dados do perfil de usuário', () => {
+                cy.visit('https://coalizao.hml.fabricadigital.com.br');
+                cy.get('.cc-btn').click();
+                cy.get('.btn-account').click();
+                cy.get('#id_username').type('mario');
+                cy.get('#id_password').type('Mario@12');
+                cy.get('.btn-primary').click();
+                cy.get(':nth-child(4) > .btn-account > .tile').click();
+                cy.get('#user-263-header-dropdown-content > [href="/profile/mario-reis-263"]').click();
+                cy.get(':nth-child(2) > .nav-link > strong').click();
+                // caso haja arquivo criado executar esta linha
+                cy.get('.list-group-item-body').click();
+                //caso seja a criação do arquivo esta linha
+                //cy.get('#lgpd-export-btn').click();
+                
+            });
+
+});
